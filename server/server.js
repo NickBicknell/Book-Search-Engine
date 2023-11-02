c3e11046021a6868e647ec21fbe205603203f7db
Nick Bicknell <nicholasbicknell@gmail.com>
2023-11-02 02:27:53 -0500

    added app.get for client/dist

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
-const db = require("./config/connection");
+
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
+
+const path = require("path");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,6 +22,10 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 };
 
 // create a new instance of Apollo Server using GraphQL schema
